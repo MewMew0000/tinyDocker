61fed96e446407330fa66aa2d19f50c2c8fa639c
MewMew <charleeeeswangj@gmail.com>
2024-10-15 17:02:17 +0900

    give a new isolated filesystem to the child process

--- main.go
+++ main.go
@@ -8,12 +8,9 @@ import (
 	"syscall"
 )
 
-// mount --make-rprivate /
-// mount -t proc proc /proc
-// ipcmk -Q
-// sudo ./tinyDocker  run /bin/sh
 func main() {
 	switch os.Args[1] {
+	//parent process
 	case "run":
 		fmt.Println("run pid", os.Getpid(), "ppid", os.Getppid())
 		initCmd, err := os.Readlink("/proc/self/exe") // get/reflect its own path (tinydocker here)
@@ -34,14 +31,38 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	//child process
 	case "init":
-		syscall.Chroot("./ubuntu2204_rootfs") // limit process to a vitrual root file system
+		pwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println("pwd", err)
+			return
+		}
+		path := pwd + "/ubuntu2204_rootfs"
+		// ensure that change in parent process will not affect the child process
+		syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+		// when path is mounted to child process, it will in the different namespace with parent process's filesystem
+		if err := syscall.Mount(path, path, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+			fmt.Println("mount fail", err)
+			return
+		}
+
+		if err := os.MkdirAll(path+"/.old", 0700); err != nil {
+			fmt.Println("mkdir fail", err)
+			return
+		}
+		// give a new isolated filesystem to the child process
+		err = syscall.PivotRoot(path, path+"/.old")
+		if err != nil {
+			fmt.Println("pivot root fail", err)
+			return
+		}
 		syscall.Chdir("/")
 		defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 		syscall.Mount("proc", "proc", "proc", uintptr(defaultMountFlags), "")
 		cmd := os.Args[2]
 		fmt.Println("exec cmd=", cmd)
-		err := syscall.Exec(cmd, os.Args[2:], os.Environ()) // replace current process with the new one
+		err = syscall.Exec(cmd, os.Args[2:], os.Environ()) // replace current process with the new one
 		if err != nil {
 			fmt.Println("exec proc fail", err)
 			return
